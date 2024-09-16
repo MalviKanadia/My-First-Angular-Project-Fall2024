acb178fd0ccf50dc40d5fd1018f4721dc58b058a
MalviKanadia <W0849019@myscc.ca>
2024-09-15 22:06:39 -0400

    working on new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from './Shared/Modules/user';
+import {JsonPipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'My-First-Angular-Project-Fall2024';
+  user1: User = {id: 1, firstName: "Diana", lastName: "Nano", department: "Programming", isAdmin: false};
+  user2: User = {id: 2, firstName: "Matt", lastName: "Haug", department: "Web Dev", isAdmin: true};
+  //Can declare values either way
+  userList: User[] = //any[] would have worked as well
+    [this.user1, this.user2,
+      {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
+      {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
+    ]
+//Function that gets called from our onclick. Takes in an
+  //arguement ofa variable called user, which is type User and returns void
+  toggleAdminStatus(user: User): void {
+    user.isAdmin = !user.isAdmin;
+  }
 }
