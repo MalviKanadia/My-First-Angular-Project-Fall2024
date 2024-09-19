a7ac4edcd422ba579899acfa16c6f1948bfd6c68
MalviKanadia <W0849019@myscc.ca>
2024-09-19 17:01:55 -0400

    working on lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {User} from './Shared/Modules/user';
-import {JsonPipe, NgForOf} from '@angular/common';
+import {User} from "./Shared/Modules/user";
+import {JsonPipe, NgForOf} from "@angular/common";
 import {StudentListComponent} from "./student-list/student-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgForOf,JsonPipe,StudentListComponent],
+  imports: [RouterOutlet, NgForOf, JsonPipe, StudentListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Student Management System';
+  title= 'User Generation';
+
+
+
+
 
 }
